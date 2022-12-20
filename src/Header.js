8f82fee6377ff639816c214425cbe46a39456afe
VitorKalil <vitorkalil@yahoo.com>
2022-12-20 15:49:22 +0100

    Primeira autalização

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+      <nav className="navbar">
+        <ul className="lista">
+          <li>Home</li>
+          <li>Sobre</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    )
+  }
